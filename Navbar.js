8c1ceec81998c57c3285456e5fbca1428a685cf8
sathya-reddy1658 <ktr6304@gmail.com>
2024-05-10 20:57:18 +0530

    added animmations

---
+++ Navbar.js
@@ -0,0 +1,11 @@
+gsap.from("#nav span",{
+  y:-100,
+  duration :1,
+  stagger:0.05,
+  scrub:1,
+
+});
+Shery.makeMagnet("#nav img", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
