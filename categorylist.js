1d0c853d0568873045370a98ca9b216f994bfcaf
sachin <sk800968@gmail.com>
2024-05-14 15:53:17 +0530

    Add-Product-Category-Image

--- categorylist.js
+++ categorylist.js
@@ -1,215 +1,83 @@
-const categoriesData = [
-    {
-        "id": "grocery",
-        "name": "Grocery",
-        "subcategories": [
-            {
-                "id": "fruits-vegetables",
-                "name": "Fruits & Vegetables",
-                "subcategories": ["Fresh Fruits", "Fresh Vegetables"]
-            },
-            {
-                "id": "dairy-eggs",
-                "name": "Dairy & Eggs",
-                "subcategories": ["Milk", "Cheese", "Eggs"]
-            },
-            {
-                "id": "bakery",
-                "name": "Bakery",
-                "subcategories": ["Bread", "Pastries", "Cakes"]
-            },
-            {
-                "id": "canned-packaged",
-                "name": "Canned & Packaged",
-                "subcategories": ["Canned Food", "Pasta", "Rice", "Snacks"]
-            },
-            {
-                "id": "desserts",
-                "name": "Desserts",
-                "subcategories": null
-            }
-        ]
-    },
-    {
-        "id": "electronics",
-        "name": "Electronics",
-        "subcategories": [
-            {
-                "id": "mobile-phones",
-                "name": "Mobile Phones",
-                "subcategories": ["Smartphones", "Feature Phones"]
-            },
-            {
-                "id": "computers",
-                "name": "Computers",
-                "subcategories": ["Laptops", "Desktops", "Tablets"]
-            },
-            {
-                "id": "accessories",
-                "name": "Accessories",
-                "subcategories": ["Headphones", "Chargers", "Cases"]
-            }
-        ]
-    },
-    {
-        "id": "clothing",
-        "name": "Clothing",
-        "subcategories": [
-            {
-                "id": "mens-clothing",
-                "name": "Men's Clothing",
-                "subcategories": ["Shirts", "Pants", "Jackets"]
-            },
-            {
-                "id": "womens-clothing",
-                "name": "Women's Clothing",
-                "subcategories": ["Dresses", "Skirts", "Tops"]
-            },
-            {
-                "id": "kids-clothing",
-                "name": "Kids' Clothing",
-                "subcategories": ["T-shirts", "Jeans", "Dresses"]
-            }
-        ]
-    },
-    {
-        "id": "home-furniture",
-        "name": "Home & Furniture",
-        "subcategories": [
-            {
-                "id": "Kitchen-appliances",
-                "name": "kitchen Appliances",
-                "subcategories": ["Blenders", "Microwaves", "Coffee Makers", "Pressure cookers", "Pans"]
-            },
-            {
-                "id": "furniture",
-                "name": "Furniture",
-                "subcategories": ["Sofas", "Tables", "Chairs", "Beds"]
-            },
-            {
-                "id": "home-decor",
-                "name": "Home Decor",
-                "subcategories": ["Rugs", "Lamps", "Mirrors", "Clocks"]
-            },
-            ,
-            {
-                "id": "pet",
-                "name": "Pet supplies",
-                "subcategories": ["Dogs", "Cats", "Fish and Aquatics"]
-            }
-        ]
-    },
-    {
-        "id": "beauty",
-        "name": "Beauty",
-        "subcategories": [
-            {
-                "id": "face",
-                "name": "Face",
-                "subcategories": ["Foundation", "Concealer", "Powder"]
-            },
-            {
-                "id": "eyes",
-                "name": "Eyes",
-                "subcategories": ["Eyeshadow", "Mascara", "Eyeliner"]
-            },
-            {
-                "id": "lips",
-                "name": "Lips",
-                "subcategories": ["Lipstick", "Lip Gloss", "Lip Liner"]
-            }
-        ]
-    },
-    {
-        "id": "travel",
-        "name": "Travel",
-        "subcategories": [
-            {
-                "id": "luggage",
-                "name": "Luggage",
-                "subcategories": ["Suitcases", "Backpacks", "Duffel Bags"]
-            },
-            {
-                "id": "travel-accessories",
-                "name": "Accessories",
-                "subcategories": ["Travel Pillows", "Travel Adapters", "Luggage Tags"]
-            },
-            {
-                "id": "travel-essentials",
-                "name": "Travel Essentials",
-                "subcategories": ["Passport Holders", "Toiletry Bags", "Travel Bottles"]
-            }
-        ]
-    },
-    {
-        "id": "sports-stationary",
-        "name": "Sports & Stationary",
-        "subcategories": [
-            {
-                "id": "fitness-equipment",
-                "name": "Fitness Equipment",
-                "subcategories": ["Dumbbells", "Resistance Bands", "Yoga Mats"]
-            },
-            {
-                "id": "sports-clothing",
-                "name": "Clothing",
-                "subcategories": ["Athletic Wear", "Running Shoes", "Sports Bras"]
-            },
-            {
-                "id": "sports-accessories",
-                "name": "Accessories",
-                "subcategories": ["Water Bottles", "Gym Bags", "Fitness Trackers"]
-            },
-            {
-                "id": "toys",
-                "name": "Toys",
-                "subcategories": null
-            },
-            ,
-            {
-                "id": "stationary",
-                "name": "Stationary",
-                "subcategories": null
-            }
-
-        ]
-    }
+let arr = [
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/flap/80/80/image/29327f40e9c4d26b.png?q=100",
+    title: "Grocery",
+    subcategories: ["Fruits & Vegetables", "Dairy & Eggs", "Bakery", "Canned & Packaged", "Desserts"]
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/flap/80/80/image/22fddf3c7da4c4f4.png?q=100",
+    title: "Mobiles",
+    subcategories: ["Smartphones", "Feature Phones"]
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/fk-p-flap/128/128/image/0d75b34f7d8fbcb3.png?q=100",
+    title: "Fashion",
+    subcategories: ["Kitchen Appliances", "Furniture", "Home Decor", "Pet supplies"],
+    href:'Product.html'
+    
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/flap/80/80/image/69c6589653afdb9a.png?q=100",
+    title: "Electronics",
+    subcategories: ["Mobile Phones", "Computers", "Accessories"]
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/flap/80/80/image/ab7e2b022a4587dd.jpg?q=100",
+    title: "Home and Furniture",
+    subcategories: ["Kitchen Appliances", "Furniture", "Home Decor", "Pet supplies"]
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/flap/80/80/image/0ff199d1bd27eb98.png?q=100",
+    title: "Appliances",
+    subcategories: ["Kitchen Appliances", "Furniture", "Home Decor", "Pet supplies"]
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/flap/80/80/image/71050627a56b4693.png?q=100",
+    title: "Travel",
+    subcategories: ["Luggage", "Accessories", "Travel Essentials"]
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/flap/80/80/image/dff3f7adcf3a90c6.png?q=100",
+    title: "Beauty, Toys and More",
+    href: "",
+    subcategories: ["Face", "Eyes", "Lips"]
+  },
+  {
+    img_src:
+      "https://rukminim1.flixcart.com/fk-p-flap/80/80/image/05d708653beff580.png?q=100",
+    title: "Two Wheelers",
+    href: "",
+    subcategories: ["Kitchen Appliances", "Furniture", "Home Decor", "Pet supplies"]
+  },
 ];
 
-$(document).ready(function () {
-    const categoriesList = $("#categories");
+arr.forEach(element => {
+  let box = document.createElement('div');
+  box.className = "category-box";
 
-    function createCategoryItem(categoryData) {
-        const categoryItem = $("<li>").text(categoryData.name);
+  let image = document.createElement('img');
+  image.src = element.img_src;
+  image.alt = element.title;
+  image.className = "category-image";
 
-        const subcategoriesList = $("<ul>");
+  let title = document.createElement("p");
+  title.className = "category-title";
+  title.innerHTML = element.title;
 
-        if (!categoryData.subcategories) {
-            const singleItem = $("<li>").text(categoryData.name);
-            subcategoriesList.append(singleItem);
-        } else {
-            $.each(categoryData.subcategories, function(index, subcategoryData) {
-                if (subcategoryData && subcategoryData.name == undefined) {
-                    const singleItem = $("<li>").text(subcategoryData);
-                    subcategoriesList.append(singleItem);
-                } else if (subcategoryData) {
-                    const subcategoryItem = createCategoryItem(subcategoryData);
-                    subcategoriesList.append(subcategoryItem);
-                }
-            });
-        }
+ 
 
-        categoryItem.append(subcategoriesList);
-        console.log(categoryItem);
+  box.appendChild(image);
+  box.appendChild(title);
 
-        return categoryItem;
-    }
 
-    categoriesData.forEach(function(categoryData) {
-        const categoryItem = createCategoryItem(categoryData);
-        categoriesList.append(categoryItem);
-    });
+
+  document.getElementById("categories").appendChild(box);
 });
-
-
-
